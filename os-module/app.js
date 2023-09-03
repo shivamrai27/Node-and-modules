34b354e7ec27006dc02d7099574c23667c03c37d
Shivam Rai <200shivamrai@gmail.com>
2023-09-04 01:45:34 +0530

    node-modules

---
+++ os-module/app.js
@@ -0,0 +1,3 @@
+const os = require('./os-module')
+
+console.log(os.uptime);
---
+++ os-module/os-module.js
@@ -0,0 +1,15 @@
+
+const os = require('os')
+
+const str = os.arch
+console.log(str);
+// const os_detailes = {
+//     name: os.userInfo.name,
+//     ver_name: os.version.name,
+//     hostname: os.hostname,
+//     uptime: os.uptime,
+//     totalMem: os.totalmem,
+//     platform: os.platform,
+// }
+// console.log(os_detailes);
+// module.exports = os_detailes;
